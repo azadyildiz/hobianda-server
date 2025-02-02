d3ff3a6a88a1eafdafa0ab60fc475010fd3831d1
Azad YILDIZ <azad.yildiz119@gmail.com>
2025-02-03 02:52:44 +0300

    ShopModule created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { ShopModule } from './shop/shop.module';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
+  imports: [ShopModule],
 })
 export class AppModule {}
