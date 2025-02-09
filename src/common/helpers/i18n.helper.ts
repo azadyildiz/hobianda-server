92bf3fcdae3c58cef92949ea6fe847c3031d1e18
Azad YILDIZ <azad.yildiz119@gmail.com>
2025-02-09 16:09:23 +0300

    typescript options changed

--- src/common/helpers/i18n.helper.ts
+++ src/common/helpers/i18n.helper.ts
@@ -1,5 +1,6 @@
 import { I18nContext } from 'nestjs-i18n';
 
 export function t(key: string) {
-  return I18nContext.current().t(key);
+  const i18n = I18nContext.current();
+  return i18n ? i18n.t(key) : key;
 }
