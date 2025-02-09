ce0f84e862e28005e486f42e2fd89ad2f8ed065a
Azad YILDIZ <azad.yildiz119@gmail.com>
2025-02-09 04:36:19 +0300

    global filters & interceptor added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
+import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Register global exception filters for HTTP and Prisma errors.
+  app.useGlobalFilters(new GlobalExceptionFilter(), new PrismaExceptionFilter());
+
+  // Register the global response interceptor to format all successful responses.
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   // Global ValidationPipe ekle
   app.useGlobalPipes(
