5993e28fff883632443c70ba123efb97ae72b78a
Azad YILDIZ <azad.yildiz119@gmail.com>
2025-02-09 15:54:00 +0300

    i18n imported to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShopModule } from './shop/shop.module';
+import { I18nModule, AcceptLanguageResolver } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ShopModule],
+  imports: [
+    ShopModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [AcceptLanguageResolver],
+    }),
+  ],
 })
 export class AppModule {}
