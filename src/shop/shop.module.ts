dea61bf99c394763d8c2f35ff7cd549c1048cf7c
Azad YILDIZ <azad.yildiz119@gmail.com>
2025-02-15 02:13:42 +0300

    jwt added to shop module

--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { ShopController } from './shop.controller';
 import { ShopService } from './shop.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [ShopController],
   providers: [ShopService, PrismaService],
 })
